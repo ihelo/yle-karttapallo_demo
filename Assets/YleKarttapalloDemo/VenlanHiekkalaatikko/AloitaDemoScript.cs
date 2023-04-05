e577f27585f8e915e771613fdbc5cd01e65d9538
venlali <venla.lipsunen@gmail.com>
2023-04-05 16:57:48 +0300

    Venlan uusi testiscene ja sisään skaalautuva demo

--- Assets/YleKarttapalloDemo/VenlanHiekkalaatikko/AloitaDemoScript.cs
+++ Assets/YleKarttapalloDemo/VenlanHiekkalaatikko/AloitaDemoScript.cs
@@ -6,12 +6,15 @@ using UnityEngine.Events;
 public class AloitaDemoScript : MonoBehaviour
 {
 
-    //public GameObject Demo;
+    public GameObject Pallo;
     public GameObject Button;
 
-    public GameObject[] objectsToScaleIn; // An array of game objects to scale in
+    public GameObject[] objectsToScaleIn1; // An array of game objects to scale in
+    public GameObject[] objectsToScaleIIn2;
+
     public float scaleInDuration = 1.0f; // The duration of the scale-in animation
     public Vector3 finalScale = new Vector3(1, 1, 1); // The final scale for the game objects
+    public Vector3 finalScale2 = new Vector3(1, 1, 1);
 
     private Vector3[] initialScales; // An array of initial scales for the game objects
 
@@ -19,11 +22,11 @@ public class AloitaDemoScript : MonoBehaviour
     void Start()
     {
 
-        initialScales = new Vector3[objectsToScaleIn.Length]; // Create a new array of initial scales for the game objects
+        initialScales = new Vector3[objectsToScaleIn1.Length]; // Create a new array of initial scales for the game objects
 
-        for (int i = 0; i < objectsToScaleIn.Length; i++)
+        for (int i = 0; i < objectsToScaleIn1.Length; i++)
         {
-            initialScales[i] = objectsToScaleIn[i].transform.localScale; // Get the initial scale for each game object
+            initialScales[i] = objectsToScaleIn1[i].transform.localScale; // Get the initial scale for each game object
         }
 
     }
@@ -36,7 +39,7 @@ public class AloitaDemoScript : MonoBehaviour
 
     public void whenButtonIsClicked()
     {
-        //Demo.SetActive(true);
+        
         //Demo.transform.localScale(0, 0, 0);
        // Button.SetActive(false);
        
@@ -48,6 +51,8 @@ public class AloitaDemoScript : MonoBehaviour
 
         Button.SetActive(false);
 
+        //Pallo.SetActive(true);
+
     }
 
 
@@ -57,11 +62,20 @@ public class AloitaDemoScript : MonoBehaviour
         while (currentTime < scaleInDuration)
         {
             float t = currentTime / scaleInDuration; // Calculate the lerp time
-            for (int i = 0; i < objectsToScaleIn.Length; i++)
+            for (int i = 0; i < objectsToScaleIn1.Length; i++)
             {
                 Vector3 initialScale = initialScales[i];
                 Vector3 newScale = Vector3.Lerp(initialScale, finalScale, t); // Calculate the new scale for each game object
-                objectsToScaleIn[i].transform.localScale = newScale;
+                objectsToScaleIn1[i].transform.localScale = newScale;
+            }
+            currentTime += Time.deltaTime; // Increment the current time
+            yield return null; // Wait for the next frame
+
+            for (int i = 0; i < objectsToScaleIIn2.Length; i++)
+            {
+                Vector3 initialScale = initialScales[i];
+                Vector3 newScale = Vector3.Lerp(initialScale, finalScale2, t); // Calculate the new scale for each game object
+                objectsToScaleIIn2[i].transform.localScale = newScale;
             }
             currentTime += Time.deltaTime; // Increment the current time
             yield return null; // Wait for the next frame
