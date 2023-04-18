3f6fa6d11df5a17afd7ef72068a881b7c4b16c71
ihelo <71507845+ihelo@users.noreply.github.com>
2023-04-18 13:18:22 +0300

    Muokattava UI ja "Liikuta kaikkea"-painike

---
+++ Assets/UIHandlesOnOff.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandlesOnOff : MonoBehaviour
+{
+    [SerializeField] private GameObject handle01Article;
+    [SerializeField] private GameObject handle02Footer;
+    [SerializeField] private GameObject handle03Video;
+    [SerializeField] private GameObject handle04Master;
+    private bool handlesActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handlesActive = false;
+    }
+
+    public void ToggleHandles()
+    {
+        if (handlesActive == false)
+        {
+            handle01Article.SetActive(true);
+            handle02Footer.SetActive(true);
+            handle03Video.SetActive(true);
+            handle04Master.SetActive(true);
+            handlesActive = true;
+            Debug.Log("Handlet p‰‰lle!");
+        }
+        else
+        {
+            handle01Article.SetActive(false);
+            handle02Footer.SetActive(false);
+            handle03Video.SetActive(false);
+            handle04Master.SetActive(false);
+            handlesActive = false;
+            Debug.Log("Handlet pois!");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
